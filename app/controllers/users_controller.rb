954e6a3b0b4fc798c88ed49225640494bd1ba7ec
rpire <rubenpire7@gmail.com>
2021-12-07 18:06:56 -0400

    Make controllers fetch information from models based on parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
-  def index; end
 
-  def show; end
+  def index
+    @users = User.order(:id)
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @posts = Post.where(author_id: @user.id)
+  end
 end
