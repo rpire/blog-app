a870c12ca051e6ac53485b628e67bcf48ff96485
rpire <rubenpire7@gmail.com>
2021-12-10 16:51:48 -0400

    Add validations to create forms

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
   belongs_to :user, foreign_key: 'author_id'
+  validates :title, presence: true
+  validates :text, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def update_comments_counter
     self.comments_counter = Comment.where(post_id: id).count
@@ -14,6 +18,6 @@ class Post < ApplicationRecord
   end
 
   def most_recent_comments
-    Comment.where(post_id: id).last(5)
+    Comment.includes(:user).where(post_id: id).last(5)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :posts
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def most_recent_posts
     Post.where(author_id: id).last(3)
