f554169359dfd8746dad37a0416d284a04fe0f76
Daniel M. Matongo <m_matongo@icloud.com>
2021-12-14 11:14:41 +0200

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2021_12_14_073753) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
